f56e7ad15a5cb9f81404ae089d26fb9545797a05
ART <arturo.gomez@tecmilenio.mx>
2025-10-04 19:42:02 -0600

    OTHER CHANGE

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ DATOS DE INICIO EN TODO.txt
 #include "pixie1.h" //Split Flap
 #include "pixie2.h" //balloon
  
-
 #define DS3231_I2C_ADDRESS 0x68 //RTC DS3231 
 
 //DHT sensor es bidireccional no conectar en solo inputs pines
